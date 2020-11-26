b04b1c4e01846cdda05a8242a58bafb3f028be0d
Jerryj1127 <Jerryj1127@gmail.com>
2020-11-26 14:25:14 +0530

    Separate functions added

--- browser.py
+++ browser.py
@@ -1,19 +1,13 @@
 #!/usr/bin/env python3
-login_page = 'https://www.udemy.com/join/login-popup/'
+
 import time
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By 
 from selenium.webdriver.support.ui import WebDriverWait 
 from selenium.webdriver.support import expected_conditions as EC 
 import os
-from creds import Creds
-from coupons import generate_udemy, scrap_udemy_coupon
-outcome = {}
-coupons = generate_udemy()
 
 
-def browser(name, url):
+def browser(driver, name, url):
     driver.get(url)
     time.sleep(5)
 
@@ -21,7 +15,7 @@ def browser(name, url):
         x_path = "//button[@class='udlite-btn udlite-btn-large udlite-btn-primary udlite-heading-md styles--btn--express-checkout--28jN4']"
         status_check = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH , x_path)))
     except:
-        return 'ERROR '+ url
+        return 'ERROR |'+ url
 
     #In case the course was already registered
     if status_check.text == 'Go to course':
@@ -36,40 +30,51 @@ def browser(name, url):
         try:
             xpath_enroll = "//button[@class='udlite-btn udlite-btn-large udlite-btn-primary udlite-heading-md styles--btn--express-checkout--28jN4'][.='Enroll now']"
             WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH , xpath_enroll))).click()
+            ##The below statement is intended for people from India but you may change it for your country
             try:
                 xpath_state = f"//*[@value][.='{Creds.state}']"
                 WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH , xpath_state))).click()
             except:
                 pass
             xpath_confirm = '//*[@id="udemy"]/div[1]/div[2]/div/div/div/div[2]/form/div[2]/div/div[4]/button'
-            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH , xpath_confirm))).click()
-             #driver.find_element_by_xpath("//button[@class='ellipsis btn btn-lg btn-primary btn-block'][.='Enroll now']").click()
+            enroll_btn = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH , xpath_confirm)))
+            time.sleep(3)
+            enroll_btn.click()
+            #driver.find_element_by_xpath("//button[@class='ellipsis btn btn-lg btn-primary btn-block'][.='Enroll now']").click()
             #driver.find_element_by_xpath('//button[normalize-space()="Enroll now"]').click()
-            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.LINK_TEXT, 'Start course')))
+            start_course = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.LINK_TEXT, 'Start course')))
+            time.sleep(1)
+            url = start_course.get_attribute('href')
             driver.save_screenshot(f"temp/{name}.png")
-            return 'Success'
+            return 'Success |' + url
         except:
-            return 'Failed '+ url
+            return 'Failed |'+ url
 
-service = Service(os.path.join(os.getcwd(),'chromedriver'))
-service.start()
-driver = webdriver.Remote(service.service_url)
-driver.get(login_page)
-driver.maximize_window()
-time.sleep(3) # Let the user actually see something!
-driver.find_element_by_xpath('//*[@id="email--1"]').send_keys(Creds.username)
-driver.find_element_by_xpath('//*[@id="id_password"]').send_keys(Creds.password)
-driver.find_element_by_xpath('//*[@id="submit-id-submit"]').click()
-time.sleep(3)
+def login(driver, Creds):
+    login_page = 'https://www.udemy.com/join/login-popup/'
+    try:
+        driver.get(login_page)
+        driver.find_element_by_xpath('//*[@id="email--1"]').send_keys(Creds.username)
+        driver.find_element_by_xpath('//*[@id="id_password"]').send_keys(Creds.password)
+        driver.find_element_by_xpath('//*[@id="submit-id-submit"]').click()
+        time.sleep(3)
+        if driver.current_url == login_page:
+            return False
+        return True
+    except: 
+        return False
 
-if not os.path.isdir('temp'):
-    os.mkdir('temp')
+def reg_course(driver, coupons):
+    if not os.path.isdir('temp'):
+        os.mkdir('temp')
+    outcome = {}
 
-for course in coupons.keys():
-    url = coupons[course]
-    outcome[course] = browser(course, url)
-driver.quit()
-print('\n\n')
+    for course in coupons.keys():
+        url = coupons[course]
+        outcome[course] = browser(driver, course, url)
+    driver.quit()
 
-for i in outcome.items():
-    print(i,'\n')
+    return outcome
+
+
+
